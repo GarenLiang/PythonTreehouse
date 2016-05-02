44d66c84d0620541c7a5ed580e79537107cc4367
JackLiang1 <ideabreeze@gmail.com>
2016-05-02 02:11:53 -0500

    Update address.py

--- address.py
+++ address.py
@@ -9,3 +9,15 @@ line=re.search(r'''
 ''',data,re.X|re.M)
 print(line)
 print(line.groupdict())
+
+
+line=re.compile(r'''
+        ^(?P<name>(?P<last>[-\w ]*,\s(?<first>[-\w ]+))\t #Last and first name
+        (?P<Email>[-\w\d.+]+@[-\w\d.]+)\t  #Email
+        (?P<Phone>\(?\d{3}\)?-?\s?\d{3}-\d{4})?\t  #Phone
+        (?P<Job>[\w\s]+,\s[\w\s.]+)\t?  #Job and company
+        (?P<Twitter>@[\w\d]+)?$   #Twitter
+''',re.X|re.M)
+
+for match in line.finditer(data):
+        print('{first} {last} <{email}>'.format(**match.grounpdict()))
